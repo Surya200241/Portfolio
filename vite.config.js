831cfb7df335d4d4b4620d479307273971332334
Surya200241 <160455944+Surya200241@users.noreply.github.com>
2025-09-25 14:11:34 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   css: {
     modules: {
-      localsConvention: 'camelCase', // Use camelCase for CSS module class names
+      localsConvention: 'camelCase', // Use camelCase for CSS module class name
     },
   },
 });
